81bc81c08eeadc06f40911f631fc9f576e1361b9
Myles Hansen <myleshunter@hotmail.com>
2020-02-22 19:41:36 -0700

    Removed Messaging Features

--- leasr-app/screens/MessagingScreen.js
+++ leasr-app/screens/MessagingScreen.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native';
-import MessagingNavigation from '../navigation/MessagingNavigation';
 
 const MessagingScreen = props => {
     return (
-        <MessagingNavigation />
+        <View style={styles.screen}>
+            <Text>Messaging Screen</Text>
+        </View>
     );
 };
 
